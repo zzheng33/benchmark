23209f02bd00b182900edff24f64820f1532e939
zzheng33 <zz695920754@gmail.com>
2024-03-11 15:30:37 -0500

    /

--- ECP/CRADL/data/filter.sh
+++ ECP/CRADL/data/filter.sh
@@ -10,7 +10,7 @@ for file in "${source_directory}/"inputs_*; do
   number=$(echo "${file}" | sed -n 's/.*inputs_\([0-9]*\)\.npy/\1/p')
 
   # Check if the number is less than 50
-  if [ "${number}" -lt 50 ]; then
+  if [ "${number}" -lt 20 ]; then
     # Move both inputs and labels files
     mv "${source_directory}/inputs_${number}.npy" "${destination_directory}/"
     mv "${source_directory}/labels_${number}.npy" "${destination_directory}/"
