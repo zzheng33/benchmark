73310e157b38e295554df6473357535b4e447eb8
zzheng33 <zz695920754@gmail.com>
2024-04-22 14:31:29 +0000

    /

--- CPU-only/NPB3.4.2/NPB3.4-OMP/MG/npbparams.h
+++ CPU-only/NPB3.4.2/NPB3.4-OMP/MG/npbparams.h
@@ -1,4 +1,4 @@
-! CLASS = E
+! CLASS = C
 !  
 !  
 !  This file is generated automatically by the setparams utility.
@@ -6,15 +6,15 @@
 !  in this directory. Do not modify it by hand.
 !  
         integer nx_default, ny_default, nz_default
-        parameter (nx_default=2048, ny_default=2048, nz_default=2048)
+        parameter (nx_default=512, ny_default=512, nz_default=512)
         integer nit_default, lm, lt_default
-        parameter (nit_default=50, lm = 11, lt_default=11)
+        parameter (nit_default=20, lm = 9, lt_default=9)
         integer debug_default
         parameter (debug_default=0)
         integer ndim1, ndim2, ndim3
-        parameter (ndim1 = 11, ndim2 = 11, ndim3 = 11)
+        parameter (ndim1 = 9, ndim2 = 9, ndim3 = 9)
         integer kind2
-        parameter (kind2=8)
+        parameter (kind2=4)
         logical  convertdouble
         parameter (convertdouble = .false.)
         character compiletime*11
