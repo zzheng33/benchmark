db09718fbfaa34fabd5566a915b8e7068b8e35d4
zzheng33 <zz695920754@gmail.com>
2024-09-18 22:48:24 +0000

    /

--- ECP/UNet/scripts/download_data.sh
+++ ECP/UNet/scripts/download_data.sh
@@ -11,12 +11,14 @@ pip install kaggle --upgrade
 
 kaggle competitions download -c carvana-image-masking-challenge -f train_hq.zip
 unzip train_hq.zip
+mkdir ./data/imgs/
 mv train_hq/* data/imgs/
 rm -d train_hq
 rm train_hq.zip
 
 kaggle competitions download -c carvana-image-masking-challenge -f train_masks.zip
 unzip train_masks.zip
+mkdir ./data/masks/
 mv train_masks/* data/masks/
 rm -d train_masks
 rm train_masks.zip
