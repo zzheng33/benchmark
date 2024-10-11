1940ba2274e8b7eb0b711d72356f06c014a45519
zzheng33 <41070207+zzheng33@users.noreply.github.com>
2024-10-10 21:51:42 -0500

    Update setup.sh

--- ECP/Resnet50/setup.sh
+++ ECP/Resnet50/setup.sh
@@ -5,6 +5,7 @@ kaggle datasets download -d deeptrial/miniimagenet
 unzip miniimagenet.zip
 wget -O ./ImageNet-Mini/synset_labels.txt \
 https://raw.githubusercontent.com/tensorflow/models/master/research/slim/datasets/imagenet_2012_validation_synset_labels.txt
+mv ./ImageNet-Mini/images ./ImageNet-Mini/train
 
 python3 imagenet_to_gcs.py \
 --raw_data_dir=./ImageNet-Mini/ \
