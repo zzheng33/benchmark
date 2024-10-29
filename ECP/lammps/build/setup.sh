d54a98ebb411e953965d8a35bc572afc5311581a
zzheng33 <41070207+zzheng33@users.noreply.github.com>
2024-10-29 12:02:56 -0500

    Update setup.sh

--- ECP/lammps/build/setup.sh
+++ ECP/lammps/build/setup.sh
@@ -2,11 +2,11 @@
 
 cmake ../cmake \
   -D BUILD_MPI=ON \
-  -D PKG_MANYBODY=yes
   -D PKG_GPU=ON \
   -D GPU_API=cuda \
   -D GPU_ARCH=sm_80 \
   -D GPU_PREC=mixed \
+  -D PKG_MANYBODY=ON
   -D CMAKE_CUDA_COMPILER=/usr/local/cuda/bin/nvcc ..
 
 make -j32
